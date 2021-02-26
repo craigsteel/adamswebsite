c2d0734ff1207bf9940b2cccbab1027ff6673103
Craig <craig@craigsteel-design.com>
2021-02-26 23:09:49 +0000

    changes to gulp file

---
+++ gulp/tasks/statics.js
@@ -0,0 +1,47 @@
+const { src, dest, parallel } = require('gulp');
+const replace = require('gulp-replace');
+
+//File paths
+const SRC_PATH = 'src';
+const DIST_PATH = 'dist';
+
+// copy HTML resources
+function htmlFn() {
+  return src(SRC_PATH + "/**/*.html")
+    .pipe(replace('dist/', ''))
+    .pipe(dest(DIST_PATH));
+}
+
+// copy the Technical Reports
+function techReportsFn() {
+  return src(SRC_PATH + "/technical-reports*/**/*", { ignore: "**/*.html" })
+    .pipe(dest(DIST_PATH));
+}
+
+// copy the Publications
+function publicationsFn() {
+  return src(SRC_PATH + "/publications*/**/*", { ignore: "**/*.html" })
+    .pipe(dest(DIST_PATH));
+}
+
+// copy the Jobs
+function jobsFn() {
+  return src(SRC_PATH + "/jobs*/**/*", { ignore: "**/*.html" })
+    .pipe(dest(DIST_PATH));
+}
+
+// other static resorces
+function othersFn() {
+  return src(SRC_PATH + "/LICENSE.txt")
+    .pipe(dest(DIST_PATH));
+}
+
+staticsFn = parallel(
+    htmlFn,
+    techReportsFn,
+    publicationsFn,
+    jobsFn,
+    othersFn
+);
+
+exports.build = staticsFn;
--- gulpfile.js
+++ gulpfile.js
@@ -1,123 +1,32 @@
-const { src, dest, watch, parallel, series } = require("gulp");
+const { task, parallel, series } = require('gulp');
 
-const eslint = require("gulp-eslint");
-const mocha = require("gulp-mocha");
-const svg = require('gulp-svg-sprite');
-const imagemin = require('gulp-imagemin');
-const imageminPngquant = require('imagemin-pngquant');
-const imageminJpegRecompress = require('imagemin-jpeg-recompress');
-const imageminWebp = require('imagemin-webp');
-const sync = require("browser-sync").create();
+const imagesTask = require('./gulp/tasks/images');
+const scriptsTask = require('./gulp/tasks/scripts');
+const stylesTask = require('./gulp/tasks/styles');
+const staticsTask = require('./gulp/tasks/statics');
+const spritesTask = require('./gulp/tasks/sprites');
+const watchTask = require('./gulp/tasks/watch');
+const cleanTask = require('./gulp/tasks/clean');
+const serverTask = require('./gulp/tasks/server');
 
-// SVG SPRITES
+task('images', imagesTask.build);
+task('scripts', scriptsTask.build);
+task('sprites', spritesTask.build);
+task('styles', stylesTask.build);
+task('statics', staticsTask.build);
+task('clean', cleanTask.build);
 
-const SPRITE_PATH = './images/svgs/*.svg';
-const SPRITE_DIST_PATH = 'public/images/svgs';
+// Simply just all the web assets
+const assetsFn = parallel(
+  stylesTask.build,
+  scriptsTask.build,
+  spritesTask.build,
+  imagesTask.build,
+  staticsTask.build
+);
+task('assets', assetsFn);
 
-function generateSVG(cb) {
-  src(SPRITE_PATH)
-    .pipe(svg({
-      shape: {
-        dimension: { // Set maximum dimensions
-          maxWidth: 100,
-          maxHeight: 100
-        },
-        spacing: { // Add padding
-          padding: 10
-        },
-        dest: "intermediate-svg"
-      },
-      mode: {
-        stack: {
-          sprite: "../sprite.svg"
-        }
-      }
-    }))
-    .pipe(dest(SPRITE_DIST_PATH));
-  cb();
-}
+task('server', series(assetsFn, serverTask.build));
+task('watch', series(assetsFn, watchTask.build));
 
-//Images
-const IMAGES_PATH = './images/png-jpg/*.{png,jpeg,jpg,gif,webp}';
-const IMAGES_DIST_PATH = 'public/images';
-
-function generateIMAGES(cb) {
-  return src(IMAGES_PATH)
-    .pipe(imagemin(
-      [
-        imagemin.gifsicle(),
-        imagemin.mozjpeg(),
-        imagemin.optipng(),
-        imageminPngquant(),
-        imageminJpegRecompress(),
-        imageminWebp({quality: 75})
-      ]
-    ))
-    .pipe(dest(IMAGES_DIST_PATH))
-    .on('end', function() {
-      cb();
-    });
-}
-
-// Fonts
-const SRC_FONTS_PATH = './webfontkit/**/*.{ttf,woff,woff2,otf,css,}';
-const DIST_FONTS_PATH = 'public/webfontkit';
-
-function generateFONTS() {
-  return src(SRC_FONTS_PATH)
-    .pipe(dest(DIST_FONTS_PATH));
-}
-
-const SRC_ICONS_PATH = './icons/**/*.{png,xml,ico,json,}';
-const DIST_ICONS_PATH = 'public/icons';
-
-function generateICONS() {
-  return src(SRC_ICONS_PATH)
-    .pipe(dest(DIST_ICONS_PATH));
-}
-
-function runLinter(cb) {
-  return src(['**/*.js', '!node_modules/**'])
-    .pipe(eslint())
-    .pipe(eslint.format())
-    .pipe(eslint.failAfterError())
-    .on('end', function() {
-      cb();
-    });
-}
-
-function runTests(cb) {
-  return src(['**/*.test.js'])
-    .pipe(mocha())
-    .on('error', function() {
-      cb(new Error('Test failed'));
-    })
-    .on('end', function() {
-      cb();
-    });
-}
-
-function watchFiles(cb) {
-  watch([ '**/*.js', '!node_modules/**'], parallel(runLinter, runTests));
-}
-
-function browserSync(cb) {
-  sync.init({
-    server: {
-      baseDir: "./public"
-    }
-  });
-
-  watch("./public/**.html").on('change', sync.reload);
-}
-
-exports.svg = generateSVG;
-exports.images = generateIMAGES;
-exports.fonts = generateFONTS;
-exports.icons = generateICONS;
-exports.lint = runLinter;
-exports.test = runTests;
-exports.watch = watchFiles;
-exports.sync = browserSync;
-
-exports.default = series(runLinter,parallel(generateSVG,generateIMAGES,generateFONTS,generateICONS,browserSync),runTests);
+task('default', assetsFn);
